709611a7cbee946ad10ec55f3b843c54c4bc9508
zOmbie1919nl <42315538+zOmbie1919nl@users.noreply.github.com>
2018-09-24 20:41:07 +0200

    Update modcom.py

--- modcom.py
+++ modcom.py
@@ -18,6 +18,14 @@ class Modcommands:
             return True
         return False
 
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CheckFailure):
+            return
+        elif isinstance(error, commands.CommandOnCooldown):
+            await ctx.send(f"The command is on a cool down. You need to wait {int(error.retry_after) +1} seconnd(s).")
+        else:
+            raise error
+
     @commands.command()
     async def clear(self, ctx, amount: int=None):
         if amount is None:
@@ -26,46 +34,6 @@ class Modcommands:
             clear = amount +1
             await ctx.channel.purge(limit=clear)
             await ctx.send(f'``{amount}`` message(s) has been cleared', delete_after = 5)
-
-    @commands.command()
-    async def userinfo(self, ctx, member: discord.Member=None):
-        if member is None:
-            member = ctx.author
-            display = member.name
-            discrim = member.discriminator
-            name = f'{display}#{discrim}'
-            joindate = f'{member.joined_at.day}-{member.joined_at.month}-{member.joined_at.year}'
-            joined = f'{joindate}, {str(member.joined_at.time())[:-10]}'
-            createddate = f'{member.created_at.day}-{member.created_at.month}-{member.created_at.year}'
-            created = f'{createddate}, {str(member.created_at.time())[:-10]}'
-            em = discord.Embed(title='userinfo', colour=0xFF0000)
-            em.set_thumbnail(url=member.avatar_url)
-            em.add_field(name='Name:', value=name, inline=False)
-            em.add_field(name='Nickname:', value=member.nick, inline=False)
-            em.add_field(name='User id', value=member.id, inline=False)
-            em.add_field(name='Status:', value=member.status, inline=False)
-            em.add_field(name='Created at:', value=created, inline=False)
-            em.add_field(name='Joined at:', value=joined, inline=False)
-            em.add_field(name='Roles:', value='\n '.join(r.name if r.name == '@everyone' else r.mention for r in sorted(member.roles, key=str)), inline=False)
-            return await ctx.send(embed=em)
-        else:
-            display = member.name
-            discrim = member.discriminator
-            name = f'{display}#{discrim}'
-            joindate = f'{member.joined_at.day}-{member.joined_at.month}-{member.joined_at.year}'
-            joined = f'{joindate}, {str(member.joined_at.time())[:-10]}'
-            createddate = f'{member.created_at.day}-{member.created_at.month}-{member.created_at.year}'
-            created = f'{createddate}, {str(member.created_at.time())[:-10]}'
-            em = discord.Embed(title='userinfo', colour=0xFF0000)
-            em.set_thumbnail(url=member.avatar_url)
-            em.add_field(name='Name:', value=name, inline=False)
-            em.add_field(name='Nickname:', value=member.nick, inline=False)
-            em.add_field(name='User id', value=member.id, inline=False)
-            em.add_field(name='Status:', value=member.status, inline=False)
-            em.add_field(name='Created at:', value=created, inline=False)
-            em.add_field(name='Joined at:', value=joined, inline=False)
-            em.add_field(name='Roles:', value='\n '.join(r.name if r.name == '@everyone' else r.mention for r in sorted(member.roles, key=str)), inline=False)
-            return await ctx.send(embed=em)
 
 
 def setup(bot):
