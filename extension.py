2e675d9f9dca5b28469b7fadecfcb7b0fe1deb03
zOmbie1919nl <42315538+zOmbie1919nl@users.noreply.github.com>
2018-10-20 09:35:48 +0200

    Update extension.py

--- extension.py
+++ extension.py
@@ -24,20 +24,5 @@ class AutoActions:
             await member.send(f"We've enabled raid mode for our server, this makes it so you cannot join the server. \nPlease try again later.")
             await member.kick(reason="Raidmode active")
 
-    async def on_message(self, msg: discord.Message):
-        if msg.channel.id == 488373296300032001:
-            guild = msg.guild
-            member = msg.author
-            role = discord.utils.get(guild.roles, id=488374525856317470)
-            role2 = discord.utils.get(msg.guild.roles, id=304833080563204096)
-            if role2 in msg.author.roles:
-                return
-            else:
-                await member.add_roles(role)
-                await member.send(f"Because of Speed-Builds, you have been muted for one minute! \nThis means you can suggest a build again in one minute!")
-                length = 60
-                await asyncio.sleep(length)
-                await member.remove_roles(role)
-
 def setup(bot):
     bot.add_cog(AutoActions(bot))
